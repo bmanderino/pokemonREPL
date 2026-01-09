48528400e19ab35079a63fbceafe0c0dfcee8f54
Brian M <3246323+bmanderino@users.noreply.github.com>
2026-01-08 17:07:12 -0800

    core repl

--- src/repl.ts
+++ src/repl.ts
@@ -1,3 +1,25 @@
+import { createInterface } from 'node:readline';
+import { stdin, stdout } from 'node:process';
+
 export function cleanInput(input: string): string[] {
   return input.trim().toLowerCase().split(" ").filter((word) => word !== "")
 }
+export function startREPL() {
+  const rl = createInterface({
+    input: stdin,
+    output: stdout,
+    prompt: "Pokedex > "
+  });
+  rl.prompt()
+  rl.on("line", (text:string) => {
+    const cleanText = cleanInput(text)
+    if (cleanText.length === 0) {
+      rl.prompt()
+    } else {
+      console.log(`Your command was: ${cleanText[0]}`)
+      rl.prompt()
+    }
+  })
+}
+
+
